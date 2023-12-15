92c1cc8dde0f13fee6330128e20b67c397748485
Daniel <dgallups@purdue.edu>
2023-12-15 01:17:02 -0500

    plagiarized nano blinky and ran on rp2400

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
 #![no_std]
+#![no_main]
 
-fn main() {}
+use arduino_nano_connect as bsp;
+use bsp::hal;
+use bsp::hal::pac;
+use bsp::hal::prelude::*;
+use embedded_hal::digital::v2::OutputPin;
+use panic_halt as _;
+
+#[arduino_nano_connect::entry]
+fn main() -> ! {
+    // Grab our singleton objects
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    //
+    // The default is to generate a 125 MHz system clock
+    let clocks = hal::clocks::init_clocks_and_plls(
+        bsp::XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    // The delay object lets us wait for specified amounts of time (in
+    // milliseconds)
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins up according to their function on this particular board
+    let pins = bsp::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut led_pin = pins.sck0.into_push_pull_output();
+
+    led_pin.set_high().unwrap();
+    delay.delay_ms(500);
+    led_pin.set_low().unwrap();
+    delay.delay_ms(500);
+    loop {}
+}
+
+// End of file
